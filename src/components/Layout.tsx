1c644653c603176388a2a11fe1c094dd38351e42
Caleb Rivera <calebrossr@gmail.com>
2023-05-11 04:20:52 -0400

    Add footer

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,5 @@
 import { Montserrat } from "next/font/google";
+import Footer from "./Footer";
 import Navbar from "./Navbar";
 
 const montserrat = Montserrat({ subsets: ["latin"], weight: "400" });
@@ -7,7 +8,8 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className={`${montserrat.className}`}>
       <Navbar />
-      <main className="mx-auto w-full max-w-screen-md px-6">{children}</main>
+      <main className="mx-auto w-full max-w-screen-lg px-6">{children}</main>
+      <Footer />
     </div>
   );
 }
