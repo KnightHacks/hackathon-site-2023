f3cb3eea9eba83dea5e847dcecfcebff3d04e5a9
Caleb Rivera <calebrossr@gmail.com>
2023-05-23 15:32:51 -0400

    Small changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body style={monteserrat.style}>
+      <body style={monteserrat.style} className="flex min-h-screen flex-col">
         <Navbar />
         <div className="px-6">{children}</div>
         <Footer />
