44a547e43b7c55a93f2b57ccf2ecd7c7efc350f8
Caleb Rivera <calebrossr@gmail.com>
2023-06-15 04:56:35 -0400

    Update edge runtime for middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { isTokenExpired } from "./utils";
 
-export const runtime = "edge";
+export const runtime = "experimental-edge";
 
 export async function middleware(req: NextRequest) {
   const url = req.nextUrl.clone();
