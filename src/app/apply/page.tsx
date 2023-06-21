120897efee9f754fb56a5f19c6d4f3f436dfaeca
Caleb Rivera <caleb.rivera@discordapp.com>
2023-06-21 12:05:20 -0400

    You've already applied

--- src/app/apply/page.tsx
+++ src/app/apply/page.tsx
@@ -6,10 +6,72 @@ export const metadata = {
   title: "Registration",
 };
 
-export default function KnightHacksRegistration() {
+const getUser = async (accessToken: string) => {
+  const query = `
+query Me {
+  me {
+    id
+    firstName
+    lastName
+    age
+    gender
+    shirtSize
+    race
+    phoneNumber
+    mailingAddress {
+      state
+      postalCode
+      country
+      city
+      addressLines
+    }
+    educationInfo {
+      major
+      name
+      level
+      graduationDate
+    }
+    email
+    applications {
+      whyAttend
+      whatDoYouWantToLearn
+      status
+    }
+  }
+}
+  `;
+
+  const res = await fetch(process.env.NEXT_PUBLIC_API_ENDPOINT!, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+    body: JSON.stringify({
+      query,
+    }),
+  });
+
+  return res.json();
+};
+
+export default async function KnightHacksRegistration() {
+  const accessToken = cookies().get("accessToken")?.value;
+
+  if (!accessToken) redirect("/sigin");
+
+  const { data } = await getUser(accessToken);
+
   return (
     <div className="mx-auto my-10 w-full max-w-screen-md px-6">
-      <KnightHacksRegistrationForm />
+      {data.applications.length > 0 ? (
+        <>
+          <div className="font-serif text-4xl font-bold">Thank you!</div>
+          <p className="mb-4">You&apos;ve already applied!</p>
+        </>
+      ) : (
+        <KnightHacksRegistrationForm />
+      )}
     </div>
   );
 }
