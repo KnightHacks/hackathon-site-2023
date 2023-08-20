d2f9d65cad624899073895bac0abdadd349f35b0
Caleb Rivera <calebrossr@gmail.com>
2023-08-19 21:47:09 -0400

    Please

--- src/app/apply/Form.tsx
+++ src/app/apply/Form.tsx
@@ -62,7 +62,7 @@ export default function KnightHacksRegistrationForm() {
       console.log(sasURL);
       console.log(data.resume);
 
-      if (data.resume) uploadResume(sasURL, data.resume);
+      if (data.resume) uploadResume(sasURL, data.resume[0]);
     }
   };
 
